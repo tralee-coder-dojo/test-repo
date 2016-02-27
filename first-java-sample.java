69c441f353238a691128c12fead41a295e21eab1
traleecoderdojo <traleecoderdojo@gmail.com>
2016-02-27 14:46:00 +0000

    Update first-java-sample.java
    Added sayNTimes

--- first-java-sample.java
+++ first-java-sample.java
@@ -1,3 +1,5 @@
+/* package codechef; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -17,6 +19,13 @@ class Person {
         System.out.println(idea);
     }
     
+    public void sayNTimes(int times) {
+        
+        for(int i = 0; i < times; i++) {
+            this.say();
+        }
+    }
+    
 }
 
 /* Name of the class has to be "Main" only if the class is public. */
@@ -26,11 +35,11 @@ class Codechef
 	{
 		Person first = new Person("I don't what to say Hello World");
 		
-		Person second = new Person();
+		Person second = new Person("I do want to say it many times");
 		
 		first.say();
 		
-		second.say();
+		second.sayNTimes(3);
 		
 		
 	}
