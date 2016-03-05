4d4a71421d9057fa62044cfac03bfc98e066300c
traleecoderdojo <traleecoderdojo@gmail.com>
2016-03-05 13:59:05 +0000

    Update first-java-sample.java

--- first-java-sample.java
+++ first-java-sample.java
@@ -19,6 +19,21 @@ class Person {
         System.out.println(idea);
     }
     
+    public int howManyCharactersInIdea() {
+        return idea.length();
+    }
+    
+    public Person biggestIdea(Person person) {
+        Person ret = null;
+        if(this.howManyCharactersInIdea() > person.howManyCharactersInIdea()) {
+            ret = this;
+        }    
+        else {
+            ret = person;
+        }
+        return ret;
+    }
+    
     public void sayNTimes(int times) {
         
         for(int i = 0; i < times; i++) {
@@ -33,6 +48,8 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+	    Person def = new Person();
+	    
 		Person first = new Person("I don't what to say Hello World");
 		
 		Person second = new Person("I do want to say it many times");
@@ -40,6 +57,13 @@ class Codechef
 		first.say();
 		
 		second.sayNTimes(3);
+		
+		System.out.println(second.howManyCharactersInIdea());
+		
+		Person bestIdea = second.biggestIdea(def);
+		
+		System.out.println("Best Idea in the world is: " );
+		bestIdea.say();
 		
 		
 	}
